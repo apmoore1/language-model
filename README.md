b6ca4f81e4844367048d0b397cb855da9f2b3f65
Andrew Moore <andrew.p.moore94@gmail.com>
2019-04-11 16:47:02 +0100

    Vocab scripts

--- README.md
+++ README.md
@@ -27,3 +27,23 @@ So from what I have gathered the reason why the test data in Wiki103 is <eos> is
 I think the next step would be to just split the yelp dataset into train, val and test based on previous work in the area on the size of train, val, and test.
 
 The next step after that would be to look at the size of the dataset by the token size where we will split based on whitespace. Then I think we will look into which model to use as the TransformerXL from what I have just gathered only uses token level information and does not encode any characters which is problematic.
+
+## How to run the Transformer ELMo model
+
+command:
+
+
+``` bash
+allennlp evaluate --cuda-device -1 -o '{"iterator": {"base_iterator": {"maximum_samples_per_batch": ["num_tokens", 500], "max_instances_in_memory": 512, "batch_size": 128 }}}' transformer-elmo-2019.01.10.tar.gz 1-billion-word-language-modeling-benchmark-r13output/heldout-monolingual.tokenized.shuffled/news.en-00000-of-00100
+```
+
+## What is the vocab comparison folder
+The vocab comparison folder contains script that will create a `vocab.json` file (or whatever you would like to call it) that will contain a list of all vocabulary terms given a corpus to create the vocabularly from and a tokeniser to `create/split` the words within the corpus.
+
+To create a vocabulary file from the 1 Billion word held out corpus:
+``` bash
+python create_vocab.py 1-Billion-Held-Out-Corpus-File-Path Vocab-File-Path whitespace
+python vocab_comparison/create_vocab.py ../1-billion-word-language-modeling-benchmark-r13output/heldout-monolingual.tokenized.shuffled/news.en-00000-of-00100 ../vocab_1_billion_held_out whitespace
+
+```
+Given the location of the 1 Billion word held out corpus it will create the json vocabulary file at the given path using the whitespace tokeniser.
---
+++ vocab_comparison/create_vocab.py
@@ -0,0 +1,123 @@
+import argparse
+from pathlib import Path
+from typing import Callable, List, Dict, Set
+import json
+import re
+
+import spacy
+from spacy.cli.download import download as spacy_download
+from spacy.cli import link
+from spacy.util import get_package_path
+from spacy.language import Language as SpacyModelType
+
+
+LOADED_SPACY_MODELS: Dict[str, SpacyModelType] = {}
+
+def _get_spacy_model(language: str) -> SpacyModelType:
+    """
+    To avoid laoding lots of spacy models the model specific to a language 
+    is loaded and saved within a Global dictionary.
+    This has been mainly taken from the `AllenNLP package <https://github.
+    com/allenai/allennlp/blob/master/allennlp/common/util.py>`_
+    :param language: Language of the SpaCy model to load.
+    :returns: The relevant SpaCy model.
+    """
+    if language not in LOADED_SPACY_MODELS:
+        disable = ['vectors', 'textcat', 'tagger', 'parser', 'ner']
+        try:
+            spacy_model = spacy.load(language, disable=disable)
+        except OSError:
+            print(f"Spacy models '{language}' not found.  Downloading and installing.")
+            spacy_download(language)
+            package_path = get_package_path(language)
+            spacy_model = spacy.load(language, disable=disable)
+            link(language, language, model_path=package_path)
+        LOADED_SPACY_MODELS[language] = spacy_model
+    return LOADED_SPACY_MODELS[language]
+
+def spacy_tokeniser(text: str) -> List[str]:
+    '''
+    `SpaCy tokeniser <https://spacy.io/>`_
+    Assumes the language to be English.
+    :param text: A string to be tokenised.
+    :returns: A list of tokens where each token is a String.
+    '''
+    spacy_model = _get_spacy_model('en')
+
+    spacy_document = spacy_model(text)
+    tokens = []
+    for token in spacy_document:
+        if not token.is_space:
+            tokens.append(token.text)
+    return tokens
+
+
+def get_tokeniser(tokeniser_name: str) -> Callable[[str], List[str]]:
+    if tokeniser_name == 'spacy':
+        print('Using the spaCy tokeniser')
+        return spacy_tokeniser
+    elif tokeniser_name == 'whitespace':
+        print('Using the WhiteSpace tokeniser')
+        return str.split
+    else:
+        raise ValueError(f'Not a recognised tokeniser name: {tokeniser_name}')
+        
+def create_vocab(text_fp: Path, tokeniser: Callable[[str], List[str]]
+                 ) -> Set[str]:
+    vocab = set()
+    with text_fp.open('r') as text_file:
+        for line in text_file:
+            line = line.strip()
+            if line:
+                tokens = tokeniser(line)
+                for token in tokens:
+                    vocab.add(token)
+    return vocab
+
+def parse_path(path_string: str) -> Path:
+    path_string = Path(path_string).resolve()
+    return path_string
+
+if __name__ == '__main__':
+    text_fp_help = 'File Path to the text file that the vocabulary is built from'
+    vocab_fp_help = 'File Path to store the created vocabulary'
+    tokeniser_help = 'Which tokeniser to use, default is spaCy'
+    from_dir_of_files_help = 'Whether or not the text File Path given is a '\
+                             'directory of text files that the vocab is to be '\
+                             'created from'
+    file_prefix_help = 'Optional filter that ensures a file is only read if '\
+                       'this is in the start of the file name e.g. `news` for'\
+                       ' files like `news.en-00001-of-00100`'
+    parser = argparse.ArgumentParser()
+    parser.add_argument("text_fp", type=parse_path, help=text_fp_help)
+    parser.add_argument("vocab_fp", type=parse_path, help=vocab_fp_help)
+    parser.add_argument("tokeniser", type=str, choices=['spacy', 'whitespace'], 
+                        default='spacy', help=tokeniser_help)
+    parser.add_argument("--from_dir_of_files", help=from_dir_of_files_help,
+                        action='store_true')
+    parser.add_argument("--file_prefix", help=file_prefix_help, type=str)
+    args = parser.parse_args()
+
+    text_fp = args.text_fp
+    vocab_fp = args.vocab_fp
+    tokeniser = get_tokeniser(args.tokeniser)
+
+    file_prefix = args.file_prefix
+    if args.from_dir_of_files:
+        vocab = set()
+        if not text_fp.is_dir():
+            raise ValueError(f'Expect the text_fp `{text_fp}` argument to be a'
+                             ' folder.')
+        text_dir: Path = text_fp
+        for text_fp in text_dir.iterdir():
+            print('done')
+    else:
+        if file_prefix:
+            if re.search(rf'^{file_prefix}', text_fp.name) is None:
+                raise FileNotFoundError(f'The file prefix `{file_prefix}` given'
+                                        ' does not match the given file name '
+                                        f'`{text_fp.name}`')
+        vocab = list(create_vocab(text_fp, tokeniser))
+    with vocab_fp.open('w+') as vocab_file:
+        json.dump(vocab, vocab_file)
+    print(f'Vocabulary has been created and written to {vocab_fp}')
